505805d67b5633f57265321348bba2affc782d7b
Jonathan Alba Videira <jonathan.videira@gmail.com>
2021-03-28 18:04:45 -0300

    First resolver

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,7 +1,13 @@
 package graph
 
+import "github.com/ajvideira/fullcycle-graphql/graph/model"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct{
+	categories []*model.Category
+	courses []*model.Course
+	chapters []*model.Chapter
+}
